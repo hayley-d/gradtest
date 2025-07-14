68e78dca821403a24f7394d3af33264ce4554d2d
Hayley Dodkins <hayleydodkins@Katlegos-MBP.localdomain>
2025-07-14 14:16:35 +0200

    add delete endpoint

--- GradTest/Endpoints/ProductEndpoint.cs
+++ GradTest/Endpoints/ProductEndpoint.cs
@@ -23,12 +23,49 @@ public static class ProductEndpoint
     public static void CreateProduct(this IEndpointRouteBuilder builder)
     {
         builder.MapPost("/product",
-            async (ApplicationDbContext ApplicationDbContext, HttpContext httpContext, ProductRequest product) =>
+            async (ApplicationDbContext ApplicationDbContext, HttpContext httpContext, ProductRequest req) =>
             {
-                Product newProduct = new Product(product);
+                Product? newProduct = new Product(req);
                 await ApplicationDbContext.Products.AddAsync(newProduct);
                 await ApplicationDbContext.SaveChangesAsync();
                 return Results.Created($"/product/{newProduct.Id}", newProduct);
             });
+    }
+
+    // Updates the product using the product ID and DTO
+    public static void UpdateProduct(this IEndpointRouteBuilder builder)
+    {
+        builder.MapPatch("/products/{id}", async (ApplicationDbContext context, ProductRequest req, Guid id) =>
+        {
+            Product? product = await context.Products.FindAsync(id);
+            if (product is null)
+            {
+                return Results.NotFound();
+            }
+            
+            product.Name = req.Name;
+            product.Price = req.Price;
+            product.Description = req.Description;
+            product.StockQuantity = req.StockQuantity;
+            
+            await context.SaveChangesAsync();
+            return Results.Ok();
+        });
+    }
+
+    // Removes a product of the speccified ID
+    public static void DeleteProduct(this IEndpointRouteBuilder builder)
+    {
+        builder.MapDelete("/products/{id}", async (ApplicationDbContext context, Guid id) =>
+        {
+            Product? product = await context.Products.FindAsync(id);
+            if (product is null)
+            {
+                return Results.NotFound();
+            }
+            context.Products.Remove(product);
+            await context.SaveChangesAsync();
+            return Results.Ok();
+        });
     }
 }
