94b753e04c85e515a3c3153c2862c4e295deca5e
hayley-d <hayley@codehesion.com>
2025-07-18 07:23:14 +0200

    GRAD-167, GRAD-163: .NET Day 2 & 3 - Add validation checks in UpdateProductCommandValidator - Remove role checks from UpdateProductCommandHandler - Add null check in UpdateProductCommandHandler

--- GradTest/Application/Products/Commands/UpdateProductCommand/UpdateProductCommandHandler.cs
+++ GradTest/Application/Products/Commands/UpdateProductCommand/UpdateProductCommandHandler.cs
@@ -1,4 +1,4 @@
-using GradTest.Persistence;
+using GradTest.Infrastructure.Persistence;
 using GradTest.Utils;
 using MediatR;
 
@@ -7,27 +7,18 @@ namespace GradTest.Application.Products.Commands.UpdateProductCommand;
 public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand>
 {
     private readonly ApplicationDbContext _dbContext;
-    private readonly IHttpContextAccessor _httpContextAccessor;
 
-    public UpdateProductCommandHandler(ApplicationDbContext dbContext, IHttpContextAccessor httpContextAccessor)
+    public UpdateProductCommandHandler(ApplicationDbContext dbContext)
     {
         _dbContext = dbContext;
-        _httpContextAccessor = httpContextAccessor;
     }
     
     public async Task Handle(UpdateProductCommand request, CancellationToken cancellationToken)
     {
-        var httpContext = _httpContextAccessor.HttpContext!;
-        await AuthenticationMiddleware.AdminAuthorize(httpContext);
-
-        if (httpContext.Response.StatusCode == StatusCodes.Status401Unauthorized)
-            throw new UnauthorizedAccessException();
-        
-        var userId = await AuthenticationMiddleware.GetUserID(httpContext);
-        if (string.IsNullOrEmpty(userId))
-            throw new UnauthorizedAccessException();
-        
         var product = await _dbContext.Products.FindAsync([request.ProductId], cancellationToken: cancellationToken);
+        
+        if (product is null)
+            throw new KeyNotFoundException("Product not found.");
             
         product.Name = request.Name;
         product.Description = request.Description;
--- GradTest/Application/Products/Commands/UpdateProductCommand/UpdateProductCommandValidator.cs
+++ GradTest/Application/Products/Commands/UpdateProductCommand/UpdateProductCommandValidator.cs
@@ -1,5 +1,6 @@
+using System.Data;
 using FluentValidation;
-using GradTest.Persistence;
+using GradTest.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 
 namespace GradTest.Application.Products.Commands.UpdateProductCommand;
@@ -13,5 +14,31 @@ public class UpdateProductCommandValidator : AbstractValidator<UpdateProductComm
          .MustAsync(async (productId, cancellation) =>
             await context.Products.AnyAsync(p => p.Id == productId, cancellation))
          .WithMessage("Product with the given ID does not exist.");
+
+      RuleFor(product => product.Name)
+         .NotEmpty().WithMessage("Product name must be provided.")
+         .MinimumLength(3).WithMessage("Product name must be at least 3 characters long.")
+         .MaximumLength(100).WithMessage("Product name must be at most 100 characters long.");
+      
+      RuleFor(product => product.Description)
+         .NotEmpty().WithMessage("Product description must be provided.")
+         .MaximumLength(350).WithMessage("Product description must be at most 350 characters long.");
+
+      RuleFor(product => product.CategoryValue)
+         .NotNull().WithMessage("Category value must be provided.")
+         .LessThanOrEqualTo(3)
+         .WithMessage("Category value must be greater than or equal to 0 and less than or equal to 3.")
+         .GreaterThanOrEqualTo(0)
+         .WithMessage("Category value must be greater than or equal to 0 and less than or equal to 3.");
+
+      RuleFor(product => product.Price)
+         .NotNull().WithMessage("Price must be provided.")
+         .GreaterThanOrEqualTo(0).WithMessage("Price must be greater than or equal to 0.")
+         .LessThan(decimal.MaxValue).WithMessage("Price value is too large.");
+         
+      RuleFor(product => product.StockQuantity)
+         .NotNull().WithMessage("Stock quantity must be provided.")
+         .GreaterThanOrEqualTo(0).WithMessage("Stock quantity must be greater than or equal to 0.")
+         .LessThan(int.MaxValue).WithMessage("Stock quantity value is too large.");
    }
 }
