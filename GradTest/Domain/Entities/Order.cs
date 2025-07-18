2ac7246ae5b5e7d683b70c9852c5d2cc47fd4305
hayley-d <hayley@codehesion.com>
2025-07-18 14:10:02 +0200

    GRAD-167 - Remove Products from order due to circular dependancy

--- GradTest/Domain/Entities/Order.cs
+++ GradTest/Domain/Entities/Order.cs
@@ -15,17 +15,12 @@ public class Order
     public DateTime OrderDate { get; init; } = DateTime.UtcNow;
     
     [Required]
-    [MinLength(1)]
-    public List<OrderProduct> Products { get; init; } = [];
-    
-    [Required]
     [Range(0.0001, double.MaxValue)]
     public required decimal ZarToUsd { get; init; }
 
     public Order(string userId, List<OrderProduct> products, decimal zar)
     {
         UserId = userId;
-        Products = products;
         ZarToUsd = zar;
     }
     
